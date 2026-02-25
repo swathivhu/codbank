c77b8c4eafe1c02c123b18d61cc239f6c4d934c3
Swathi Virupaksh <swathipv.2501@gmail.com>
2026-02-25 10:34:38 +0000

    Restore login route

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+      <div style={{color: "white", background: "black", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <h1>Login Page Working ✅</h1>
+      </div>
+    );
+  }
