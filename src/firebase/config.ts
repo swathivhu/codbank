4597f0c9f6427427561ecb5c5f785582d99103c9
Swathi Virupaksh <swathipv.2501@gmail.com>
2026-02-25 10:10:58 +0000

    Fix API key

---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+// This file contains the Firebase configuration.
+// IMPORTANT: Replace the placeholder values with your own Firebase project's configuration.
+// You can find this in your Firebase project's settings.
+
+export const firebaseConfig = {
+  "projectId": "studio-8074874472-5f29c",
+  "appId": "1:1068759800785:web:da7126f2a0c383c3f25ef0",
+  "apiKey": "AIzaSyC9ARmOc2WyJR0LC-9iiEKnoWzLjwv9mCM",
+  "authDomain": "studio-8074874472-5f29c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1068759800785"
+};
